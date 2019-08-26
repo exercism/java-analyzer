59dd9062cfe37b646eddf4bf15fd491c9eb91a7c
jmrunkle <jmrunkle@google.com>
2019-08-25 20:56:22 -0700

    Add a sampling of passing archives to verify

--- src/main/java/analyzer/Statistics.java
+++ src/main/java/analyzer/Statistics.java
@@ -8,7 +8,10 @@ import analyzer.exercises.twofer.Twofer;
 import analyzer.exercises.hamming.Hamming;
 import org.json.JSONArray;
 import org.json.JSONObject;
-   
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 
 import com.google.common.collect.HashMultimap;
@@ -22,6 +25,7 @@ public class Statistics {
         Multiset<String> failComments = HashMultiset.create();
         Multiset<String> passComments = HashMultiset.create();
         SetMultimap<String, Integer> archiveByComment = HashMultimap.create();
+        List<Integer> passingArchives = new ArrayList<>();
 
         String slug = args.length > 0 ? args[0] : "hamming";
         Function<String, Exercise> constructor;
@@ -35,12 +39,16 @@ public class Statistics {
                 constructor = dir -> new Twofer(dir, WriteAnalysisToFile.NO);
         }
 
-        for (int i = 0; i < 500; i++) {
-            Exercise ex = constructor.apply("archive/" + slug + "/" + i + "/");
+        for (int archive = 0; archive < 500; archive++) {
+            Exercise ex = constructor.apply("archive/" + slug + "/" + archive + "/");
             ex.parse();
             JSONObject analysis = ex.getAnalysis();
             String status = analysis.get("status").toString();
             statuses.add(status);
+
+            if (status.equals("approve")) {
+                passingArchives.add(archive);
+            }
 
             if (!analysis.has("comments")) {
                 continue;
@@ -54,7 +62,7 @@ public class Statistics {
                 } else {
                     comment = ((JSONObject) commentObj).get("comment").toString();
                 }
-                archiveByComment.put(comment, i);
+                archiveByComment.put(comment, archive);
                 if (status.equals("disapprove")) {
                     failComments.add(comment);
                 } else if (status.equals("approve")) {
@@ -71,6 +79,8 @@ public class Statistics {
         System.out.println(formatMultisetWithNewLines(passComments));
         System.out.printf("%n===> ARCHIVE SAMPLES BY COMMENT:%n%n");
         sampleArchivesAndPrint(archiveByComment);
+        System.out.printf("%n===> PASSING ARCHIVE SAMPLES:%n%n");
+        samplePassingArchivesAndPrint(passingArchives);
     }
 
     private static String formatMultisetWithNewLines(
@@ -80,16 +90,26 @@ public class Statistics {
 
     /** Grabs up to 5 samples for each comment for easy debugging. */
     private static void sampleArchivesAndPrint(
-        SetMultimap<String, Integer> multimap) {
+        SetMultimap<String, Integer> archivesByComment) {
         System.out.printf("{%n");
-        multimap.keySet().forEach(
+        archivesByComment.keySet().forEach(
             comment ->
                 System.out.printf(
                     " %s=%s,%n",
                     comment,
-                    multimap.get(comment).stream()
+                    archivesByComment.get(comment).stream()
                         .limit(5)
+                        .sorted()
                         .collect(toImmutableList())));
         System.out.printf("}%n");
+    }
+
+    private static void samplePassingArchivesAndPrint(List<Integer> passingArchives) {
+        if (passingArchives.isEmpty()) {
+            System.out.println("No passing archives.");
+            return;
+        }
+        Collections.shuffle(passingArchives);
+        System.out.println(passingArchives.stream().limit(5).sorted().collect(toImmutableList()));
     }
 }
