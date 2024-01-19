a41b6c22b63367c8366be3e3b0fc9d9d6bd9d9bc
Sander Ploegsma <sanderploegsma@gmail.com>
2024-01-19 15:09:47 +0100

    Add comment types to Twofer comments

--- src/main/java/analyzer/exercises/twofer/UseTernaryOperator.java
+++ src/main/java/analyzer/exercises/twofer/UseTernaryOperator.java
@@ -1,6 +1,7 @@
 package analyzer.exercises.twofer;
 
 import analyzer.Comment;
+import analyzer.CommentType;
 
 /**
  * @see <a href="https://github.com/exercism/website-copy/blob/main/analyzer-comments/java/two-fer/use_ternary_operator.md">Markdown Template</a>
@@ -9,5 +10,10 @@ class UseTernaryOperator extends Comment {
     @Override
     public String getKey() {
         return "java.two-fer.use_ternary_operator";
+    }
+
+    @Override
+    public CommentType getType() {
+        return CommentType.ACTIONABLE;
     }
 }
