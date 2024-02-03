41393555445b8a314e3bd4d7a461f2549e1737ac
Sander Ploegsma <sanderploegsma@gmail.com>
2024-02-03 20:11:14 +0100

    Remove checks for CLI argument paths ending with /

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ Then, run the Java analyzer using `build/libs/java-analyzer.jar`.
 For example, to analyze a solution for the `leap` exercise, run:
 
 ```sh
-java -jar build/libs/java-analyzer.jar leap /path/to/leap/ /path/to/output/folder/
+java -jar build/libs/java-analyzer.jar leap /path/to/leap /path/to/output/folder
 ```
 
-The analyzer output is written to `analysis.json` and `tags.json` in `/path/to/output/folder/`.
+The analyzer output is written to `analysis.json` and `tags.json` in `/path/to/output/folder`.
 
 ### Running with Docker
 
@@ -39,10 +39,10 @@ Then, run the image and mount the directory of the solution to analyze.
 For example, to analyze a solution for the `leap` exercise located at `~/exercism/java/leap`, run:
 
 ```sh
-docker run -v /path/to/leap:/input -v /path/to/output/folder:/output exercism/java-analyzer leap /input/ /output/
+docker run -v /path/to/leap:/input -v /path/to/output/folder:/output exercism/java-analyzer leap /input /output
 ```
 
-The analyzer output is written to `analysis.json` and `tags.json` in `/path/to/output/folder/`.
+The analyzer output is written to `analysis.json` and `tags.json` in `/path/to/output/folder`.
 
 ## Tests
 
--- src/main/java/analyzer/AnalyzerCli.java
+++ src/main/java/analyzer/AnalyzerCli.java
@@ -9,13 +9,19 @@ import java.nio.file.Path;
  * The CLI expects three arguments and is used like this:
  *
  * <pre>
- * java -jar java-analyzer.jar exercise-slug /path/to/input/ /path/to/output/
+ * java -jar java-analyzer.jar exercise-slug /path/to/input /path/to/output
  * </pre>
  */
 public class AnalyzerCli {
 
-    private static boolean isNotValidDirectory(String p) {
-        return !p.endsWith("/") || !new File(p).isDirectory();
+    private static Path validateDirectory(String directory) {
+        var file = new File(directory);
+
+        if (!file.exists() || !file.isDirectory()) {
+            throw new IllegalArgumentException("Not a valid directory: " + directory);
+        }
+
+        return file.toPath();
     }
 
     public static void main(String... args) throws IOException {
@@ -24,21 +30,12 @@ public class AnalyzerCli {
             System.exit(-1);
         }
 
-        String slug = args[0];
-        String inputDirectory = args[1];
-        String outputDirectory = args[2];
+        var slug = args[0];
+        var inputDirectory = validateDirectory(args[1]);
+        var outputDirectory = validateDirectory(args[2]);
 
-        if (isNotValidDirectory(inputDirectory)) {
-            System.err.println("Invalid input directory. Must be a valid directory and end with a slash.");
-            System.exit(-1);
-        }
-        if (isNotValidDirectory(outputDirectory)) {
-            System.err.println("Invalid output directory. Must be a valid directory and end with a slash.");
-            System.exit(-1);
-        }
-
-        var outputWriter = new OutputWriter(Path.of(outputDirectory));
-        var solution = new SubmittedSolution(slug, Path.of(inputDirectory));
+        var outputWriter = new OutputWriter(outputDirectory);
+        var solution = new SubmittedSolution(slug, inputDirectory);
         var output = AnalyzerRoot.analyze(solution);
         outputWriter.write(output);
     }
