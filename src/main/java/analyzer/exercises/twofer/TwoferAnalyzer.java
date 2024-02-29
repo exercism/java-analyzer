a810853fb7de2ff041362e18866d501dc5f523a6
Manuel Maxera <95315128+manumafe98@users.noreply.github.com>
2024-02-29 15:05:16 -0300

    Updating ``Two Fer`` Analyzer to use our new analyzer convention (#137)
    * Updating Two Fer Analyzer to use our new analyzer convention
    
    * Deleting whitespace
    
    * Updating usage of prefer string concatenation as now it is a general comment

--- src/main/java/analyzer/exercises/twofer/TwoferAnalyzer.java
+++ src/main/java/analyzer/exercises/twofer/TwoferAnalyzer.java
@@ -1,33 +1,86 @@
 package analyzer.exercises.twofer;
 
 import analyzer.OutputCollector;
+
+import java.util.List;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
 import analyzer.Analyzer;
 import analyzer.Solution;
 import analyzer.comments.AvoidHardCodedTestCases;
+import analyzer.comments.PreferStringConcatenation;
 
 /**
  * The {@link TwoferAnalyzer} is the analyzer implementation for the {@code two-fer} practice exercise.
  *
  * @see <a href="https://github.com/exercism/java/tree/main/exercises/practice/two-fer">The two-fer exercise on the Java track</a>
  */
-public class TwoferAnalyzer implements Analyzer {
+public class TwoferAnalyzer extends VoidVisitorAdapter<OutputCollector> implements Analyzer {
 
     @Override
     public void analyze(Solution solution, OutputCollector output) {
-        TwoferWalker walker = new TwoferWalker();
-
-        solution.getCompilationUnits().forEach(cu -> cu.walk(walker));
-
-        if (walker.hasHardCodedTestCases) {
-            output.addComment(new AvoidHardCodedTestCases());
-        } else if (walker.usesFormat) {
-            output.addComment(new AvoidStringFormat());
-        } else if (walker.returnCount > 1) {
-            output.addComment(new UseOneReturn());
-        } else {
-            if (walker.usesIfStatement) {
-                output.addComment(new UseTernaryOperator());
-            }
+        for (CompilationUnit compilationUnit : solution.getCompilationUnits()) {
+            compilationUnit.accept(this, output);
         }
+    }
+
+    @Override
+    public void visit(MethodDeclaration node, OutputCollector output) {
+        if (hasHardCodedTestCases(node)) {
+            output.addComment(new AvoidHardCodedTestCases());
+            return;
+        }
+
+        if (hasIfStatement(node)) {
+            output.addComment(new UseTernaryOperator());
+        }
+
+        if (hasMoreThanOneReturnStatement(node)) {
+            output.addComment(new UseOneReturn());
+        }
+
+        if (callsFormatMethod(node)) {
+            output.addComment(new PreferStringConcatenation());
+        }
+    }
+
+    private static boolean hasHardCodedTestCases(MethodDeclaration node) {
+        List<StringLiteralExpr> hardcodedStrings = node.findAll(StringLiteralExpr.class,
+                x -> x.getValue().contains("Alice") || x.getValue().contains("Bob"));
+
+        return hardcodedStrings.size() > 1;
+    }
+
+    private static boolean hasMoreThanOneReturnStatement(MethodDeclaration node) {
+        long returnStmtCount = node.getBody()
+                .map(body -> body.getStatements().stream().filter(TwoferAnalyzer::isReturnStatement).count())
+                .orElse(0L);
+        
+        return returnStmtCount > 1;
+    }
+
+    private static boolean callsFormatMethod(MethodDeclaration node) {
+        return !node.findAll(MethodCallExpr.class, x -> x.getNameAsString().contains("format")).isEmpty();
+    }
+
+    private static boolean hasIfStatement(MethodDeclaration node) {
+        return node.getBody().map(body -> body.getStatements().stream().anyMatch(TwoferAnalyzer::isIfStatement))
+                .orElse(false);
+    }
+
+    private static boolean isIfStatement(Statement statement) {
+        return !statement.findAll(IfStmt.class).isEmpty();
+    }
+
+    private static boolean isReturnStatement(Statement statement) {
+        return !statement.findAll(ReturnStmt.class).isEmpty();
     }
 }
