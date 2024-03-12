de8fe89ec8de68310f598d6646b692f86cc553ff
manumafe98 <manumaxerapro@gmail.com>
2024-03-12 17:54:36 -0300

    Fixing smoke tests

--- tests/wizards-and-warriors/todos-not-removed/src/main/java/Fighter.java
+++ tests/wizards-and-warriors/todos-not-removed/src/main/java/Fighter.java
@@ -9,33 +9,41 @@ class Fighter {
     }
 }
 
+// TODO: define the Warrior class
 class Warrior extends Fighter {
 
+    @Override
     public String toString() {
         return "Fighter is a Warrior";
     }
 
+    @Override
     public boolean isVulnerable() {
         return false;
     }
 
+    @Override
     int getDamagePoints(Fighter target) {
         return target.isVulnerable() ? 10 : 6;
     }
 }
 
+// TODO: define the Wizard class
 class Wizard extends Fighter {
 
     boolean isSpellPrepared = false;
 
+    @Override
     public String toString() {
         return "Fighter is a Wizard";
     }
 
+    @Override
     boolean isVulnerable() {
         return !isSpellPrepared;
     }
 
+    @Override
     int getDamagePoints(Fighter target) {
         return isSpellPrepared ? 12 : 3;
     }
